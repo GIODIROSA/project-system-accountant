b9cd20db625a44ec90d815d1ad628104d7c42f14
giovanni di rosa <gdirosa@flagare.cl>
2025-11-02 17:03:01 -0300

    [ADD] watch, node module, type module, morgen, dependecy and script run project

--- backennd-system-accountant/src/index.js
+++ backennd-system-accountant/src/index.js
@@ -1,11 +1,5 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Este es mi primer servidor en Node.js con Express');
-});
-
-app.listen(port, () => {
-  console.log(`Servidor ejecutándose en el puerto ${port}`);
-});
+app.listen(3000)
+console.log('Server is running on port 3000');
