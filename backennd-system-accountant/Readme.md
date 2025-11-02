ffeddab411886d4305b948142e721cee8d030744
giovanni di rosa <gdirosa@flagare.cl>
2025-11-02 17:48:26 -0300

    [ADD] config server basic

---
+++ backennd-system-accountant/Readme.md
@@ -0,0 +1,2 @@
+# Nodejs backennd-system-accountant
+
--- backennd-system-accountant/src/index.js
+++ backennd-system-accountant/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
+import {PORT} from './config.js';
 
 const app = express();
-app.listen(3000)
-console.log('Server is running on port 3000');
+app.listen(PORT)
+console.log(`Server is running on port ${PORT}`);
